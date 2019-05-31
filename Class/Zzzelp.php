f98c4c072ad2cffd18bdc68942d159d2c4920861
Moswil971 <51216940+Moswil971@users.noreply.github.com>
2019-05-31 02:00:20 +0200

    Update Zzzelp.php

--- Class/Zzzelp.php
+++ Class/Zzzelp.php
@@ -3,10 +3,10 @@
 class Zzzelp {
 
 	public static $url;
-	public static $sel_hash = '1*f6_D'; // A MODIFIER A L'INSTALLATION
+	public static $sel_hash = '2*f6_S'; // A MODIFIER A L'INSTALLATION
 	public static $IP;
 	public static $page;
-	public static $pseudo_admin = 'delangle' ;  // A MODIFIER A L'INSTALLATION
+	public static $pseudo_admin = 'moswil' ;  // A MODIFIER A L'INSTALLATION
 	public static $onglet;
 	public static $url_site = 'http://zzzelp.fr/';
 	public static $url_page;
